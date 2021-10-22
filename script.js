1f9c1ee8becf70d4c6a9a71e47428ef9258db10f
Shoozle <sean@seanmurtagh.uk>
2021-10-22 03:55:51 +0100

    Canvas drawings for each stage and clear lost

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+/** @type{HTMLCanvasElement}*/
+
 const wordsToGuess = [
     'Aberystwyth',
     'Merthyr Tydfil',
@@ -7,11 +9,17 @@ const wordsToGuess = [
     'Dyffryn Ardudwy'
 ]
 
+
+
 const buttonArea = document.querySelector('.letter__buttons');
 const wordArea = document.querySelector('.hangmanword');
 const hangmanLivesArea = document.querySelector('.hangmanlives');
+const canvas = document.querySelector('.hangmancanvas');
+const ctx = canvas.getContext('2d');
+ctx.strokePath = 'black';
 const letters = 'abcdefghijklmnopqrstuvwxyz'.toUpperCase();
-let currentLives = 6;
+let currentLives = 8;
+let currentStage = -1;
 let wordToGuess;
 let secretWord;
 let word;
@@ -19,9 +27,58 @@ let word;
 hangmanLivesArea.textContent = currentLives;
 wordArea.textContent = secretWord;
 
+const drawStage = (stage) => {
+    switch (stage) {
+        case 0: {
+            ctx.fillRect(20, 20, 5, 200);
+            break;
+        }
+        case 1: {
+            ctx.fillRect(20, 20, 100, 5);
+            break;
+        }
+        case 2: {
+            ctx.fillRect(120, 20, 5, 20);
+            break;
+        }
+        case 3: {
+            ctx.beginPath();
+            ctx.arc(123, 55, 15, 0, 2 * Math.PI, true);
+            ctx.stroke();
+            break;
+        }
+        case 4: {
+            ctx.fillRect(120, 68, 5, 30);
+            break; 
+        }
+        case 5: {
+            ctx.fillRect(105, 80, 35, 5);
+            break; 
+        }
+        case 6: {
+            ctx.beginPath();
+            ctx.lineWidth = 5;
+            ctx.moveTo(122, 95);
+            ctx.lineTo(140, 110);
+            ctx.fill();
+            ctx.stroke();
+            break; 
+        }
+        case 7: {
+            ctx.beginPath();
+            ctx.lineWidth = 5;
+            ctx.moveTo(122, 95);
+            ctx.lineTo(102, 110);
+            ctx.fill();
+            ctx.stroke();
+            break; 
+        }
+    }
+}
+
 const displayLetters = () => {
     const lettersArray = [...letters];
-    const letterElements = lettersArray.map(letter => 
+    const letterElements = lettersArray.map(letter =>
         `<button class="letter__btn" id="${letter}" onClick="checkLetter(${letter})">${letter}</button>`
     ).join('');
     buttonArea.innerHTML = letterElements;
@@ -36,7 +93,7 @@ const newWordToGuess = () => {
 
 const revealLetter = (letter) => {
     const oldWord = [...secretWord]
-    for (let i=0; i <word.length; i++) {
+    for (let i = 0; i < word.length; i++) {
         if (word[i] === letter) {
             oldWord[i] = letter
         }
@@ -53,14 +110,14 @@ const revealLetter = (letter) => {
 
 const addFail = () => {
     currentLives--;
-
+    currentStage++;
     if (currentLives == 0) {
         setTimeout(() => {
-            alert('You died')
+            alert('We lost them')
             resetGame();
         }, 250);
     }
-
+    drawStage(currentStage);   
     hangmanLivesArea.textContent = currentLives;
 }
 
@@ -73,7 +130,7 @@ const checkLetter = (letter) => {
     } else {
         addFail();
     }
-    
+
 }
 
 const resetGame = () => {
@@ -82,6 +139,7 @@ const resetGame = () => {
     hangmanLivesArea.textContent = currentLives;
     buttonArea.querySelectorAll('*').forEach(n => n.remove());
     displayLetters();
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
 }
 
 newWordToGuess();
